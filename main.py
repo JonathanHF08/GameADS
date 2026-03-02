6a6f0f1e246b66eb9f911472b0998efdaa2362e2
Jonathan <jonathanhf08022001@gmail.com>
2026-03-02 20:45:17 -0300

    adicionando imagens e criando menu

--- main.py
+++ main.py
@@ -1,17 +1,4 @@
+from code.Game import Game
 
-import pygame
-
-
-pygame.init()
-
-Window = pygame.display.set_mode(size = (600, 480))
-print('Setup end')
-
-print('Setup start')
-
-while True:
-    # Check for all events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit() #Close window
-            quit() #end pygame
+game = Game()
+game.run()
