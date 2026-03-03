106fd50346eec50c101c503d60ae2b9c0df1a33c
Jonathan <jonathanhf08022001@gmail.com>
2026-03-03 19:55:17 -0300

    Menu pronto

--- code/Game.py
+++ code/Game.py
@@ -1,17 +1,21 @@
 
 import pygame
 
+from asset.Const import WIN_WIDTH, WIN_HEIGHT
 from code.Menu import Menu
 
 class Game:
+
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self, ):
+
         while True:
             menu = Menu(self.window)
             menu.run()
+            pass
 
           #Check for all events
             #for event in pygame.event.get():
